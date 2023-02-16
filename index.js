9ecc5e2bc3370f17352caaf140a2117577631729
SShrimpman <camaraoacsn@gmail.com>
2023-02-16 16:51:43 +0000

    Multithreading

--- index.js
+++ index.js
@@ -85,10 +85,11 @@ window.addEventListener("resize", () => {
 const loader = new IFCLoader();
 
 loader.ifcManager.setWasmPath("wasm/");
+loader.ifcManager.useWebWorkers( true, "./IFCWorker.js");
 
-loader.ifcManager.setupThreeMeshBVH(computeBoundsTree, disposeBoundsTree, acceleratedRaycast);
+// loader.ifcManager.setupThreeMeshBVH(computeBoundsTree, disposeBoundsTree, acceleratedRaycast);
 
-const ifcModels = [];
+// const ifcModels = [];
 
 const input = document.getElementById('file-input');
 input.addEventListener('change', async () => {
@@ -96,101 +97,112 @@ input.addEventListener('change', async () => {
   const url = URL.createObjectURL(file);
   const model = await loader.loadAsync(url);
   scene.add(model);
-  ifcModels.push(model);
+  // ifcModels.push(model);
 })
 
-const raycaster = new Raycaster();
-raycaster.firstHitOnly = true;
-const mouse = new Vector2();
+setupProgress();
 
-function cast(event) {
-  const bounds = canvas.getBoundingClientRect();
-  
-
-  const x1 = event.clientX - bounds.left;
-  const x2 = bounds.right - bounds.left;
-  mouse.x = (x1 / x2) * 2 - 1;
-
-  const y1 = event.clientY - bounds.top;
-  const y2 = bounds.bottom - bounds.top;
-  mouse.y = -(y1 / y2) * 2 + 1;
-  
-  raycaster.setFromCamera(mouse, camera);
-
-  return raycaster.intersectObjects(ifcModels)[0];
+function setupProgress(){
+  const text = document.getElementById('progress-text');
+  loader.ifcManager.setOnProgress((event) => {
+    const percent = event.loaded / event.total * 100;
+    const formatted = Math.trunc(percent);
+    text.innerText = formatted;
+  })
 }
 
-const highLightMaterial = new MeshBasicMaterial({
-  transparent: true,
-  opacity: 0.6,
-  color: 0xff88ff,
-  depthTest: false,
-})
+// const raycaster = new Raycaster();
+// raycaster.firstHitOnly = true;
+// const mouse = new Vector2();
 
-const selectionMaterial = new MeshBasicMaterial({
-  transparent: true,
-  opacity: 0.8,
-  color: 0xff22ff,
-  depthTest: false,
-})
-
-let lastModel;
-
-async function pick(event, material, getProps) {
-  const found = cast(event);
-
-  if( found ) {
-    const index = found.faceIndex;
-    lastModel = found.object;
-    const geometry = found.object.geometry;
-    const id = loader.ifcManager.getExpressId( geometry, index);
-
-    if( getProps ) {
-      const buildings = await loader.ifcManager.getAllItemsOfType(found.object.modelID, IFCBUILDING, true);
-      const building = buildings[0];
-      console.log(building);
-
-      // const buildingProps = await loader.ifcManager.getItemProperties(found.object.modelID, buildingID, true);
-      // console.log(buildingProps);
-
-
-      // const props = await loader.ifcManager.getItemProperties(found.object.modelID, id);
-      // console.log(props);
-      // const pSets = await loader.ifcManager.getPropertySets(found.object.modelID, id);
-      // console.log(pSets);
-
-      // // const realValues = [];
-      // for(const pSet of pSets) {
-      //   const realValues = [];
-
-      //   for(const prop of pSet.HasProperties) {
-      //     const id = prop.value;
-      //     const value = await loader.ifcManager.getItemProperties(found.object.modelID, id);
-      //     realValues.push(value);
-      //   }
-      //   pSet.HasProperties = realValues;
-      //   // pSet.HasProperties = [...realValues];
-      // }
-      // console.log(pSets);
-    }
-
-    loader.ifcManager.createSubset({
-      modelID: found.object.modelID,
-      material: material,
-      ids: [id],
-      scene,
-      removePrevious: true
-    });
-  } else if (lastModel) {
+// function cast(event) {
+//   const bounds = canvas.getBoundingClientRect();
   
-    loader.ifcManager.removeSubset(lastModel.modelID, material);
-    lastModel = undefined;
 
-  }
-}
+//   const x1 = event.clientX - bounds.left;
+//   const x2 = bounds.right - bounds.left;
+//   mouse.x = (x1 / x2) * 2 - 1;
 
-// canvas.ondblclick = function(event) {
-//   pick(event);
+//   const y1 = event.clientY - bounds.top;
+//   const y2 = bounds.bottom - bounds.top;
+//   mouse.y = -(y1 / y2) * 2 + 1;
+  
+//   raycaster.setFromCamera(mouse, camera);
+
+//   return raycaster.intersectObjects(ifcModels)[0];
 // }
-canvas.onmousemove = (event) => pick( event, highLightMaterial, false);
-canvas.ondblclick = (event) => pick( event, selectionMaterial, true);
+
+// const highLightMaterial = new MeshBasicMaterial({
+//   transparent: true,
+//   opacity: 0.6,
+//   color: 0xff88ff,
+//   depthTest: false,
+// })
+
+// const selectionMaterial = new MeshBasicMaterial({
+//   transparent: true,
+//   opacity: 0.8,
+//   color: 0xff22ff,
+//   depthTest: false,
+// })
+
+// let lastModel;
+
+// async function pick(event, material, getProps) {
+//   const found = cast(event);
+
+//   if( found ) {
+//     const index = found.faceIndex;
+//     lastModel = found.object;
+//     const geometry = found.object.geometry;
+//     const id = loader.ifcManager.getExpressId( geometry, index);
+
+//     if( getProps ) {
+//       const buildings = await loader.ifcManager.getAllItemsOfType(found.object.modelID, IFCBUILDING, true);
+//       const building = buildings[0];
+//       console.log(building);
+
+//       // const buildingProps = await loader.ifcManager.getItemProperties(found.object.modelID, buildingID, true);
+//       // console.log(buildingProps);
+
+
+//       // const props = await loader.ifcManager.getItemProperties(found.object.modelID, id);
+//       // console.log(props);
+//       // const pSets = await loader.ifcManager.getPropertySets(found.object.modelID, id);
+//       // console.log(pSets);
+
+//       // // const realValues = [];
+//       // for(const pSet of pSets) {
+//       //   const realValues = [];
+
+//       //   for(const prop of pSet.HasProperties) {
+//       //     const id = prop.value;
+//       //     const value = await loader.ifcManager.getItemProperties(found.object.modelID, id);
+//       //     realValues.push(value);
+//       //   }
+//       //   pSet.HasProperties = realValues;
+//       //   // pSet.HasProperties = [...realValues];
+//       // }
+//       // console.log(pSets);
+//     }
+
+//     loader.ifcManager.createSubset({
+//       modelID: found.object.modelID,
+//       material: material,
+//       ids: [id],
+//       scene,
+//       removePrevious: true
+//     });
+//   } else if (lastModel) {
+  
+//     loader.ifcManager.removeSubset(lastModel.modelID, material);
+//     lastModel = undefined;
+
+//   }
+// }
+
+// // canvas.ondblclick = function(event) {
+// //   pick(event);
+// // }
+// canvas.onmousemove = (event) => pick( event, highLightMaterial, false);
+// canvas.ondblclick = (event) => pick( event, selectionMaterial, true);
